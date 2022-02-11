77ba6b245d2cddda9f458beb662190f3f3c309f6
CarolinaSanchezP <84477369+CaroSanchez0620@users.noreply.github.com>
2022-02-11 16:04:43 -0600

    Update user-list.component.ts

--- front-generate-reports/src/app/Components/user-list/user-list.component.ts
+++ front-generate-reports/src/app/Components/user-list/user-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {QrScannerComponent} from 'angular2-qrscanner';
+import * as XLSX from 'xlsx';
+
+// declaracion de array para lectura de excel
+type AOA= any [][] ;
 
 @Component({
   selector: 'app-user-list',
@@ -14,6 +18,12 @@ export class UserListComponent implements OnInit {
   activeUser: String = 'true'
   showScanner: Boolean = false
   QRResult: any = ''
+
+  //inicio de array para el archivo excel
+  data: AOA = [
+    [1, 2],
+    [3, 4],
+  ];
 
   constructor(
     private router: Router  ) { }
@@ -67,6 +77,32 @@ export class UserListComponent implements OnInit {
       this.QRResult = result
       this.goToLink(result)
     })
+  }
+
+  //codigo para lectura de archivo en formato excel o csv
+  onFileChange(evt:any){
+    
+    //coneccion al lector de archivos
+
+    const target: DataTransfer= <DataTransfer>(evt.target);
+
+    if(target.files.length !== 1) throw new Error('No se pueden usar multiples archivos');
+    const reader: FileReader = new FileReader();
+
+  reader.onload=(e:any)=>{
+    //lee el libro de trabajo
+    const bstr:string=e.target.result;
+    const wb : XLSX.WorkBook=XLSX.read(bstr,{type:'binary'});
+    //toma la primera hoja
+    const wsname:string=wb.SheetNames[0];
+    const ws:XLSX.WorkSheet=wb.Sheets[wsname];
+    console.log(ws);
+    //guarda los datos
+    this.data=<AOA>(XLSX.utils.sheet_to_json(ws,{header:1}));
+    console.log(this.data);
+    };
+    reader.readAsBinaryString(target.files[0]);
+    
   }
 
 
