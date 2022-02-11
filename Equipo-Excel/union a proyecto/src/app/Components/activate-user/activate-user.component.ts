d39980b8c8bdc452dcaa1261432fe5c3dbaed146
CarolinaSanchezP <84477369+CaroSanchez0620@users.noreply.github.com>
2022-02-11 01:13:16 -0600

    Add files via upload

---
+++ Equipo-Excel/union a proyecto/src/app/Components/activate-user/activate-user.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { EncryptionService } from 'src/app/encryption.service';
+import { ServicesClienteService } from './services-cliente.service';
+import * as CryptoJS from 'crypto-js';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-activate-user',
+  templateUrl: './activate-user.component.html',
+  styleUrls: ['./activate-user.component.scss']
+})
+
+export class ActivateUserComponent implements OnInit {
+  title = 'Login';
+  secretKey:string = '';
+  userId: any = 'Hola'
+  public ICliente!: Welcome;
+  public IEmpleaod!: NumeroEmpleado;
+
+  constructor(
+    public api: ServicesClienteService,
+    public service: EncryptionService,
+    private activatedRoute: ActivatedRoute
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.userId = this.activatedRoute.snapshot.params.id
+    console.log('userId: ', this.userId)
+    this.secretKey = this.service.obtenerClaveSecreta('Sandra', 'Francisco')
+    // Encriptar
+    let encrypted = CryptoJS.AES.encrypt("HolaMund0!", this.secretKey);
+    console.log('encrypted: ', encrypted)
+
+    // Desencriptar
+    var decrypted = CryptoJS.AES.decrypt(encrypted, this.secretKey);
+    console.log('decrypted: ', decrypted)
+  }
+
+
+
+  public  ValidarCampos():void{
+    const correo = (document.getElementById("txtCorroe") as HTMLInputElement).value;
+    localStorage.removeItem('datos');
+    this.api.ObtenerCliente(correo).subscribe((res:Welcome)=>{
+      console.log(res.datos[0].email);
+        if(res.datos[0].email === undefined){
+          alert("Usuario No Existe");
+        }
+        this.ICliente=res;
+        localStorage.setItem('datos',JSON.stringify(this.ICliente));
+    });
+  }
+  public EnviarCorreo():void{
+   // this.ICliente=JSON.parse(localStorage.getItem('datos') || '');
+    const nempelado = (document.getElementById("txtCorroe") as HTMLInputElement).value;
+    this.IEmpleaod={
+      NEmpleado:nempelado
+    }
+    this.api.ActualizarEmpleado(this.IEmpleaod).subscribe();
+    alert('Actualizado Correctamente');
+  }
+
+}
+export interface Dato {
+  email: string;
+  nombre: string;
+  puesto: string;
+  correoInstitucional: string;
+  autorizacion: boolean;
+  token: string;
+}
+
+interface  NumeroEmpleado{
+  NEmpleado: string;
+}
+
+interface Welcome {
+  ok:    boolean;
+  datos: Dato[];
+}
